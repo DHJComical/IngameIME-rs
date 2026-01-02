988dfa8752a6605101b753edd8dbfb0a31d850d8
Windmill_City <1449182174@qq.com>
2026-01-02 21:34:07 +0800

    基本egui界面

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use egui::{Context, ViewportId, Visuals};
+use egui_wgpu::{Renderer, RendererOptions, ScreenDescriptor};
+use egui_winit::State;
 use log::{debug, info};
 use std::{error::Error, sync::Arc};
 use wgpu::{
@@ -76,6 +79,10 @@ impl<'a> Wgpu<'a> {
 struct WinitApp<'a> {
     wgpu: Option<Wgpu<'a>>,
     window: Option<Arc<Window>>,
+
+    egui: Context,
+    state: Option<State>,
+    renderer: Option<Renderer>,
 }
 
 impl<'a> ApplicationHandler for WinitApp<'a> {
@@ -89,15 +96,33 @@ impl<'a> ApplicationHandler for WinitApp<'a> {
         info!("Creating WGPU for window");
         let wgpu = pollster::block_on(Wgpu::new(window.clone().into()));
 
+        info!("Creating egui state");
+        let state = State::new(
+            self.egui.clone(),
+            ViewportId::default(),
+            window.as_ref(),
+            Some(window.scale_factor() as f32),
+            None,
+            None,
+        );
+
+        info!("Creating egui renderer");
+        let renderer = Renderer::new(&wgpu.device, wgpu.config.format, RendererOptions::default());
+
         info!("Showing application window");
         window.set_visible(true);
 
         self.wgpu = Some(wgpu);
         self.window = Some(window);
+        self.state = Some(state);
+        self.renderer = Some(renderer);
         info!("Application resumed");
     }
 
     fn window_event(&mut self, el: &ActiveEventLoop, _: WindowId, event: WindowEvent) {
+        let state = self.state.as_mut().unwrap();
+        let window = self.window.as_ref().unwrap();
+
         match event {
             WindowEvent::CloseRequested => {
                 el.exit();
@@ -109,23 +134,29 @@ impl<'a> ApplicationHandler for WinitApp<'a> {
                 }
             }
             WindowEvent::RedrawRequested => {
-                if let Some(wgpu) = &mut self.wgpu {
-                    if wgpu.config.width == 0 || wgpu.config.height == 0 {
-                        return;
-                    }
+                let wgpu = self.wgpu.as_mut().unwrap();
 
-                    let output = wgpu.surface.get_current_texture().unwrap();
-                    let view = output
-                        .texture
-                        .create_view(&TextureViewDescriptor::default());
+                // 跳过零尺寸渲染
+                if wgpu.config.width == 0 || wgpu.config.height == 0 {
+                    return;
+                }
 
-                    let mut encoder =
-                        wgpu.device
-                            .create_command_encoder(&CommandEncoderDescriptor {
-                                label: Some("Render Encoder"),
-                            });
+                // 获取当前纹理视图
+                let output = wgpu.surface.get_current_texture().unwrap();
+                let view = output
+                    .texture
+                    .create_view(&TextureViewDescriptor::default());
 
-                    let render_pass = encoder.begin_render_pass(&RenderPassDescriptor {
+                // 初始化命令编码器
+                let mut encoder = wgpu
+                    .device
+                    .create_command_encoder(&CommandEncoderDescriptor {
+                        label: Some("Render Encoder"),
+                    });
+
+                // 场景绘制
+                {
+                    let rpass = encoder.begin_render_pass(&RenderPassDescriptor {
                         label: Some("Render Pass"),
                         color_attachments: &[Some(RenderPassColorAttachment {
                             view: &view,
@@ -143,10 +174,82 @@ impl<'a> ApplicationHandler for WinitApp<'a> {
                         })],
                         ..Default::default()
                     });
-                    drop(render_pass);
-                    wgpu.queue.submit(Some(encoder.finish()));
-                    output.present();
+                    drop(rpass);
                 }
+
+                // egui 绘制
+                {
+                    let renderer = self.renderer.as_mut().unwrap();
+
+                    let input = state.take_egui_input(&window);
+                    let full_output = self.egui.run(input, |ctx| {
+                        // 使用亮色主题
+                        ctx.set_visuals(Visuals::light());
+
+                        let window = egui::Window::new("Hello egui with WGPU!");
+                        window.show(ctx, |ui| {
+                            let text = String::from("Hello World!");
+                            ui.label(text);
+                        });
+                    });
+
+                    // 图元信息
+                    let primitives = self
+                        .egui
+                        .tessellate(full_output.shapes, full_output.pixels_per_point);
+
+                    // 屏幕尺寸
+                    let screen_desc = ScreenDescriptor {
+                        size_in_pixels: [wgpu.config.width, wgpu.config.height],
+                        pixels_per_point: full_output.pixels_per_point,
+                    };
+
+                    // 更新纹理和缓冲区
+                    {
+                        for (id, image_delta) in &full_output.textures_delta.set {
+                            renderer.update_texture(&wgpu.device, &wgpu.queue, *id, image_delta);
+                        }
+                        renderer.update_buffers(
+                            &wgpu.device,
+                            &wgpu.queue,
+                            &mut encoder,
+                            &primitives,
+                            &screen_desc,
+                        );
+                    }
+                    // 绘制图元
+                    {
+                        let mut rpass = encoder
+                            .begin_render_pass(&RenderPassDescriptor {
+                                label: Some("egui Render Pass"),
+                                color_attachments: &[Some(RenderPassColorAttachment {
+                                    view: &view,
+                                    resolve_target: None,
+                                    depth_slice: None,
+                                    ops: Operations {
+                                        load: LoadOp::Load,
+                                        store: StoreOp::Store,
+                                    },
+                                })],
+                                ..Default::default()
+                            })
+                            .forget_lifetime();
+                        renderer.render(&mut rpass, &primitives, &screen_desc);
+                    }
+                    // 清理不再使用的纹理
+                    {
+                        for id in &full_output.textures_delta.free {
+                            renderer.free_texture(id);
+                        }
+                    }
+                    // 检查是否需要重绘
+                    if self.egui.has_requested_repaint() {
+                        window.request_redraw();
+                    }
+                }
+
+                wgpu.queue.submit(Some(encoder.finish()));
+                output.present();
             }
             WindowEvent::KeyboardInput {
                 event:
@@ -165,7 +268,11 @@ impl<'a> ApplicationHandler for WinitApp<'a> {
                     }
                 }
             }
-            _ => (),
+            event => {
+                if state.on_window_event(&window, &event).repaint {
+                    window.request_redraw();
+                }
+            }
         }
     }
 }
